a5e6dd4ea8e3922e1e44dea09ee3cbc61c88992f
Isaac <izzatazimov958@gmail.com>
2023-02-12 18:40:42 +0500

    added "directory" component which renders "category-items"

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -1,21 +1,24 @@
-import './category-item.styles.scss';
+import './category-item.styles.scss'
+
+import React from 'react'
 
 const CategoryItem = ({ category }) => {
-  const { imageUrl, title } = category;
+  const { imageUrl, title } = category
   return (
-    <div className='category-container'>
+    <div className="category-container">
+      {/* img */}
       <div
-        className='background-image'
+        className="background-image"
         style={{
           backgroundImage: `url(${imageUrl})`,
         }}
-      />
-      <div className='category-body-container'>
+      ></div>
+      <div className="category-body-container">
         <h2>{title}</h2>
         <p>Shop Now</p>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default CategoryItem;
+export default CategoryItem
